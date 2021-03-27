4fddbd2e0493486113d1640aa9079538f54d3696
isabela-coder <81197670+isabela-coder@users.noreply.github.com>
2021-03-27 00:19:16 -0300

    Aula 22-03 - Pacotes MVC criados e Formulario CADPESSOA

---
+++ src/exercicio10/Exercicio10.java
@@ -0,0 +1,24 @@
+
+package exercicio10;
+
+
+public class Exercicio10 {
+
+   
+    public static void main(String[] args) {
+        
+        
+        
+ double base, altura, areaRetangulo;
+ 
+ base = 10.34;
+ altura =20;
+ areaRetangulo = base * altura;
+ 
+ System.out.println("A area de um retangulo de base = " + base);
+ System.out.println("e altura " + altura);
+ System.out.println("é " + areaRetangulo);
+ 
+ }
+}
+    
